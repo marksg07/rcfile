dced6326a044ac3fe48551bca39642f3e922f780
Gabriel Marks <gabriel_marks@brown.edu>
2019-05-17 14:45:50 -0400

    Epic shit

--- my.lua
+++ my.lua
@@ -3,6 +3,314 @@ function print_a()
     crawl.mpr("hello i am my.rc")
 end
 mk = 1
+_path = nil
+_seqForward = nil
+_seqBackward = nil
+_seqidx = 0
+_dir = true
+_better = false
+_xoff = 0
+_yoff = 0
+
+function doPillarAbove()
+    doPillar(0, -1)
+end
+
+function doPillar(x, y)
+    killPillar()
+    doSearch(x, y)
+    doStart()
+end
+
+function doStart()
+    if _path == nil then
+        crawl.mpr("No search selected!")
+        return
+    end
+    local x = _xoff
+    local y = _yoff
+    local path = _path
+    local idx = 0
+    for i, xy in ipairs(path) do
+        if xy[1] == x and xy[2] == y then
+            idx = i
+            break
+        end
+    end
+    if idx == 0 then
+        crawl.mpr("Please step on one of the excluded tiles.")
+        return
+    end
+    stopStepTracker()
+    startPillarDance()
+    _path = nil
+    _seqForward = getSeq(path)
+    _seqBackward = getSeqBackwards(path)
+    _seqidx = idx
+end
+do
+    local toOff
+    function getOffset(cmd)
+        toOff = toOff or {CMD_MOVE_UP_LEFT={-1, -1},
+            CMD_MOVE_UP_RIGHT={1, -1},
+            CMD_MOVE_DOWN_LEFT={-1, 1},
+            CMD_MOVE_DOWN_RIGHT={1, 1},
+            CMD_MOVE_UP={0, -1},
+            CMD_MOVE_DOWN={0, 1},
+            CMD_MOVE_LEFT={-1, 0},
+            CMD_MOVE_RIGHT={1, 0} }
+        return toOff[cmd]
+    end
+end
+
+
+function doNextAction()
+    if not shouldDance() then
+        killPillar()
+        return
+    end
+    assert(_seqForward ~= nil)
+    assert(_seqBackward ~= nil)
+    assert(_seqidx ~= 0)
+    local monsters = getAllMonsters()
+    for i, mon in ipairs(monsters) do
+        if string.find(mon:speed_description(), "fast") then
+            crawl.mpr("Fast monster in LOS!")
+            killPillar()
+            return
+        end
+        if #(mon:spells()) ~= 0 or mon:has_known_ranged_attack() then
+            crawl.mpr("Spellcaster/ranged/abilityperson in LOS!")
+            killPillar()
+            return
+        end
+        if mon:status("fast") or mon:status("covering ground quickly") then
+            crawl.mpr("Hasted/berserked/swifting monster in LOS!")
+            killPillar()
+            return
+        end
+    end
+
+    crawl.mpr("Num monsters: " .. #monsters)
+    local x, y, xdir, ydir, xndir, yndir
+    if _dir then
+        local xy = getOffset(_seqForward[_seqidx])
+        x = xy[1]
+        y = xy[2]
+        xdir, ydir = x, y
+    else
+        assert(not _dir)
+        local xy = getOffset(_seqBackward[_seqidx])
+        x = xy[1]
+        y = xy[2]
+        xdir, ydir = x, y
+    end
+    if not tileIsBetter(x, y, monsters) then
+        crawl.mpr("Prev direction not better, checking backwards")
+        if not _dir then
+            local xy = getOffset(_seqForward[_seqidx])
+            x = xy[1]
+            y = xy[2]
+            xndir, yndir = x, y
+        else
+            assert(_dir)
+            local xy = getOffset(_seqBackward[_seqidx])
+            x = xy[1]
+            y = xy[2]
+            xndir, yndir = x, y
+        end
+        _dir = not _dir
+        if not tileIsBetter(x, y, monsters) then
+            crawl.mpr("Neither direction better, checking forwards")
+            _dir = not _dir
+            x, y = xdir, ydir
+            if not tileIsGood(x, y, monsters) then
+                crawl.mpr("Forwards not good, trying backwards")
+                _dir = not _dir
+                x, y = xndir, yndir
+                if not tileIsGood(x, y, monsters) then
+                    crawl.mpr("No good choices to walk!")
+                    killPillar()
+                    return
+                end
+            end
+        end
+    end
+    local nextAction
+    if _dir then
+        nextAction = _seqForward[_seqidx]
+        _seqidx = _seqidx + 1
+        if _seqidx > #_seqForward then
+            _seqidx = 1
+        end
+    else
+        assert(not _dir)
+        nextAction = _seqBackward[_seqidx]
+        _seqidx = _seqidx - 1
+        if _seqidx < 1 then
+            _seqidx = #_seqBackward
+        end
+
+    end
+    crawl.do_commands({nextAction})
+end
+
+_arb = 0
+function shouldDance()
+    if _arb < 100 then
+        _arb = _arb + 1
+        return true
+    end
+    _arb = 0
+    return false
+end
+
+_trackSteps = false
+_pillarDance = false
+
+function startStepTracker()
+    _trackSteps = true
+    _xoff = 0
+    _yoff = 0
+end
+
+function stopStepTracker()
+    _trackSteps = false
+end
+
+function startPillarDance()
+    _pillarDance = true
+end
+
+function stopPillarDance()
+    _pillarDance = false
+end
+
+function killPillar()
+    stopStepTracker()
+    stopPillarDance()
+    _path = nil
+    _xoff = 0
+    _yoff = 0
+    _seqForward = nil
+    _seqBackward = nil
+    _seqidx = nil
+    _dir = true
+    _better = false
+end
+
+function ch_start_running(kind)
+    if _trackSteps and kind ~= nil then
+        crawl.mpr("Any form of autotravel (including shift+dir running) does not work with pillar dancing!")
+        killPillar()
+    end
+end
+
+function ready()
+    if _pillarDance then
+        doNextAction()
+    end
+end
+
+function moveUpLeft()
+    move(-1, -1, "CMD_MOVE_UP_LEFT")
+end
+
+function moveUpRight()
+    move(1, -1, "CMD_MOVE_UP_RIGHT")
+end
+
+function moveDownLeft()
+    move(-1, 1, "CMD_MOVE_DOWN_LEFT")
+end
+
+function moveDownRight()
+    move(1, 1, "CMD_MOVE_DOWN_RIGHT")
+end
+
+function moveDown()
+    move(0, 1, "CMD_MOVE_DOWN")
+end
+
+function moveUp()
+    move(0, -1, "CMD_MOVE_UP")
+end
+
+function moveLeft()
+    move(-1, 0, "CMD_MOVE_LEFT")
+end
+
+function moveRight()
+    move(1, 0, "CMD_MOVE_RIGHT")
+end
+
+function move(x, y, cmd)
+    crawl.do_commands({cmd})
+    if _trackSteps then
+        _moved = true
+        _xoff = _xoff + x
+        _yoff = _yoff + y
+    end
+end
+
+function is_candidate_for_attack(m)
+    --if m then crawl.mpr("Checking: (" .. x .. "," .. y .. ") " .. m:name()) end
+    if not m or m:attitude() ~= 0 then
+        return false
+    end
+    if m:name() == "butterfly"
+            or m:name() == "orb of destruction" then
+        return false
+    end
+    if m:is_firewood() then
+        --crawl.mpr("... is firewood.")
+        if string.find(m:name(), "ballistomycete") then
+            return true
+        end
+        return false
+    end
+    return true
+end
+
+function getAllMonsters()
+    local monsters = {}
+    local los = you.los()
+    for x_off=-los,los do
+        for y_off=-los,los do
+            if view.cell_see_cell(0, 0, x_off, y_off) then
+                local mon = monster.get_monster_at(x_off, y_off)
+                if is_candidate_for_attack(mon) then
+                    monsters[#monsters + 1] = mon
+                end
+            end
+        end
+    end
+    return monsters
+end
+
+function tileIsGood(x, y, monsters)
+    for i, mon in ipairs(monsters) do
+        if getDist(0, 0, mon:x_pos(), mon:y_pos()) > getDist(x, y, mon:x_pos(), mon:y_pos()) then
+            if view.cell_see_cell(x, y, mon:x_pos(), mon:y_pos()) then
+                return false
+            end
+        end
+    end
+    return true
+end
+
+function tileIsBetter(x, y, monsters)
+    for i, mon in ipairs(monsters) do
+        if getDist(0, 0, mon:x_pos(), mon:y_pos()) >= getDist(x, y, mon:x_pos(), mon:y_pos()) then
+            if view.cell_see_cell(x, y, mon:x_pos(), mon:y_pos()) then
+                return false
+            end
+        end
+    end
+    return true
+end
+
+
 function doSearch(x, y)
     local pillar = create(x, y)
     if pillar == nil then
@@ -17,7 +325,7 @@ function doSearch(x, y)
     -- toHide[{x2, y2}] = true
     -- hideTiles(toHide)
     -- crawl.mpr("hiding tiles: " .. x1 .. ", " .. y1 .. " -- " .. x2 .. ", " .. y2)
-    path = getPath(tileset, x1, y1, x2, y2)
+    local path = getPath(tileset, x1, y1, x2, y2)
     local pathTiles = {}
     for i, t in ipairs(path) do
         pathTiles[t] = true
@@ -37,7 +345,7 @@ function doSearch(x, y)
         mk.put(tileset, next[1], y1, false)
         mk.put(tileset, next[1], y1-1, false)
     end
-    path2 = getPath(tileset, x1, y1, x2, y2)
+    local path2 = getPath(tileset, x1, y1, x2, y2)
     pathTiles = {}
     for i, t in ipairs(path2) do
         pathTiles[t] = true
@@ -52,12 +360,14 @@ function doSearch(x, y)
     for i, t in ipairs(path) do
         pathTiles[t] = true
     end
-    showTiles(pathTiles)
-    printTiles(path)
-    seq = getSeq(path)
-    for i, n in ipairs(seq) do
-        crawl.mpr(n)
-    end
+    --showTiles(pathTiles)
+    --printTiles(path)
+    --seq = getSeq(path)
+    --for i, n in ipairs(seq) do
+    --  crawl.mpr(n)
+    --end
+    _path = path
+    startStepTracker()
     return path
 end
 
@@ -65,9 +375,9 @@ function getSeq(path)
     local seq = {}
     for i, s in ipairs(path) do
         local t = path[(i + 1)]
-        crawl.mpr("asdasde")
+        --crawl.mpr("asdasde")
         if i == #path then
-            crawl.mpr("EZZZZZZZZZZZZ")
+            --crawl.mpr("EZZZZZZZZZZZZ")
             t = path[1]
         end
         local n = nil
@@ -76,6 +386,37 @@ function getSeq(path)
         elseif t[1]==s[1] and t[2]==s[2]-1 then
             n = "CMD_MOVE_UP"
         elseif t[1]==s[1]+1 and t[2]==s[2] then 
+            n = "CMD_MOVE_RIGHT"
+        elseif t[1]==s[1]-1 and t[2]==s[2] then
+            n = "CMD_MOVE_LEFT"
+        elseif t[1]==s[1]+1 and t[2]==s[2]+1 then
+            n = "CMD_MOVE_DOWN_RIGHT"
+        elseif t[1]==s[1]+1 and t[2]==s[2]-1 then
+            n = "CMD_MOVE_UP_RIGHT"
+        elseif t[1]==s[1]-1 and t[2]==s[2]+1 then
+            n = "CMD_MOVE_DOWN_LEFT"
+        elseif t[1]==s[1]-1 and t[2]==s[2]-1 then
+            n = "CMD_MOVE_UP_LEFT"
+        end
+        seq[i] = n
+    end
+    return seq
+end
+
+function getSeqBackwards(path)
+    local seq = {}
+    for i, s in ipairs(path) do
+        local t = path[(i - 1)]
+        --crawl.mpr("asdasde")
+        if i == 1 then
+            t = path[#path]
+        end
+        local n = nil
+        if t[1]==s[1] and t[2]==s[2]+1 then
+            n = "CMD_MOVE_DOWN"
+        elseif t[1]==s[1] and t[2]==s[2]-1 then
+            n = "CMD_MOVE_UP"
+        elseif t[1]==s[1]+1 and t[2]==s[2] then
             n = "CMD_MOVE_RIGHT"
         elseif t[1]==s[1]-1 and t[2]==s[2] then
             n = "CMD_MOVE_LEFT"
@@ -206,11 +547,11 @@ function getNeighboringType(tiles, type)
     for xy, _ in pairs(tiles) do
         local x = xy[1]
         local y = xy[2]
-        crawl.mpr("Processing tile " .. x .. ", " .. y)
+        --crawl.mpr("Processing tile " .. x .. ", " .. y)
         for oxoy, _ in pairs(n_pos) do
             local off_x = oxoy[1]
             local off_y = oxoy[2]
-            crawl.mpr("Tile bordering is of type " .. view.feature_at(x+off_x, y+off_y))
+            --crawl.mpr("Tile bordering is of type " .. view.feature_at(x+off_x, y+off_y))
             if travel.feature_traversable(view.feature_at(x+off_x, y+off_y), type) then
                 neighbors[{x+off_x, y+off_y}] = true
             end
@@ -335,28 +676,30 @@ function getPath(tileset, x1, y1, x2, y2)
         if trav then
             mk.put(dist, x, y, 10000)
             mk.put(nodes, x, y, true)
-            crawl.mpr("set: x = " .. x .. " y = " .. y)
+            --crawl.mpr("set: x = " .. x .. " y = " .. y)
         end
     end
     mk.put(dist, x1, y1, 0)
     while next(nodes) ~= nil do
         local u = getMinTile(nodes, dist)
         mk.put(nodes, u[1], u[2], nil)
-        crawl.mpr("Getting next: x = " .. u[1] .. " y = " .. u[2])
+        --crawl.mpr("Getting next: x = " .. u[1] .. " y = " .. u[2])
         if u[1] == x2 and u[2] == y2 then
             if mk.get(prev, u[1], u[2]) == nil then
                 crawl.mpr("PANIC U NOT IN PREV")
+                assert(false)
             end
             return parsePrev(prev, x1, y1, x2, y2)
         end
         for v, _ in pairs(getNeighboring(nodes, u[1], u[2])) do
-            crawl.mpr("Neighbor: x = " .. v[1] .. " y = " .. v[2])
+            --crawl.mpr("Neighbor: x = " .. v[1] .. " y = " .. v[2])
             local altDist = mk.get(dist, u[1], u[2]) + 1
             if mk.get(dist, v[1], v[2]) == nil or altDist < mk.get(dist, v[1], v[2]) then
                 mk.put(dist, v[1], v[2], altDist)
                 mk.put(prev, v[1], v[2], u)
                 if u == nil then
                     crawl.mpr("WTF")
+                    assert(false)
                 end
             end
         end
@@ -370,10 +713,7 @@ function parsePrev(prev, x1, y1, x2, y2)
     local u = {x2, y2}
     path[1] = u
     while not (u[1] == x1 and u[2] == y1) do
-        crawl.mpr("Adding to path: " .. u[1] .. ", " .. u[2])
-        if mk.get(prev, u[1], u[2]) == nil then 
-            crawl.mpr("WHY THIO ")
-        end
+        --crawl.mpr("Adding to path: " .. u[1] .. ", " .. u[2])
         u = mk.get(prev, u[1], u[2])
         if u == nil then
             crawl.mpr("BAD PATH!")
