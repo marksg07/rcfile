f0e0f5860361c4fdc3007418feb05eb3e07d9be0
Gabriel Marks <gabriel_marks@brown.edu>
2019-12-16 13:30:54 -0500

    Fix script

--- mkrc.py
+++ mkrc.py
@@ -2,7 +2,7 @@
 import os
 
 with open('crawl.rc') as f1:
-    with open('pillardance.lua') as f2:
+    with open('pillardance/pillardance.lua') as f2:
         with open('out.rc', 'w') as f3:
             f3.write(f1.read())
             f3.write('\n{\n')
